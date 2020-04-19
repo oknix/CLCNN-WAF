199ca5db25a23184121136c9502204b8b749b961
Kei Ochiai <ochi-kei@akane.waseda.jp>
2020-04-19 18:29:45 +0900

    modified programs

--- input/csic2010_csv/make_csv_test.py
+++ input/csic2010_csv/make_csv_test.py
@@ -1,30 +1,37 @@
-f = open("anomalousTrafficTest.txt", "r")
+import csv
 
-raw_text = ""
-data_list = []
-before_line = "initial"
+def make_csv(filename, flag):
+    with open(filename, "r") as f:
+        raw_text = ""
+        data_list = []
+        before_line = "initial"
 
-while (True):
-    line = f.readline()
-    if line:
-        if line == '\n' and "Connection:" in before_line:
-            data_list.append({1:"{0}".format(raw_text)})
-            raw_text = ""
-            before_line = line
-        elif ("GET" not in line) and ("POST" not in line) and line != '\n' and before_line == '\n':
-            raw_text += line
-            data_list.append({1:"{0}".format(raw_text)})
-            raw_text = ""
-            before_line = line
-        else:
-            if line == '\n' and before_line == '\n':
-                pass
-            elif line == '\n' and "Content-Length:" not in before_line:
-                pass
+        while True:
+            line = f.readline()
+            if line:
+                if line == '\n' and "Connection:" in before_line:
+                    data_list.append(["{0}".format(flag),"{0}".format(raw_text)])
+                    raw_text = ""
+                    before_line = line
+                elif ("GET" not in line) and ("POST" not in line) and ("PUT" not in line) and line != '\n' and before_line == '\n':
+                    raw_text += line
+                    data_list.append(["{0}".format(flag),"{0}".format(raw_text)])
+                    raw_text = ""
+                    before_line = line
+                elif line == '\n' and before_line == '\n':
+                    pass
+                elif line == '\n' and "Content-Length:" not in before_line:
+                    pass
+                else:
+                    raw_text += line
+                    before_line = line
             else:
-                raw_text += line
-                before_line = line
-    else:
-        break
+                break
 
-print(data_list)
+    with open(filename.replace("txt", "csv"), "w") as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        writer.writerows(data_list)
+
+if __name__ == "__main__":
+    make_csv("anomalousTrafficTest.txt", "1")
+    make_csv("normalTrafficTraining.txt", "0")
